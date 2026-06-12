818b881d549b3631067927d6079b578eb025a2d4
aleksandra-gur <aleksandragurnak@gmail.com>
2026-06-13 01:10:52 +0300

    изменения 2

--- main.py
+++ main.py
@@ -7,3 +7,12 @@ from profile import print_profile
 
 print("Student Branching App")
 print_profile()
+
+
+# main.py после подключения subjects.py
+from profile import print_profile
+from subjects import print_subjects
+
+print("Student Branching App")
+print_profile()
+print_subjects()
